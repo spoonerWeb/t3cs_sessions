2e3eb4900b88358b3dc7c466d97bbb18badbe860
Thomas Löffler <loeffler@spooner-web.de>
2016-05-09 12:05:23 +0200

    [TASK] PSR-2 in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,25 +10,25 @@
  * "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Sessions',
-	'description' => '',
-	'category' => 'plugin',
-	'author' => 'Thomas Löffler',
-	'author_email' => 'loeffler@spooner-web.de',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => '1',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '2.1.0',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Sessions',
+    'description' => '',
+    'category' => 'plugin',
+    'author' => 'Thomas Löffler',
+    'author_email' => 'loeffler@spooner-web.de',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '1',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '2.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '6.2.0-7.6.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
