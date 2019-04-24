3d4ac7bb4df44ea23f2a66256dc7af228a79780a
Thomas Löffler <loeffler@spooner-web.de>
2019-04-24 22:40:40 +0200

    Remove old class calls and modernizes code

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,27 +1,21 @@
 <?php
-use T3CS\T3csSessions\Command\NotificationCommandController;
-use T3CS\T3csSessions\Task\NotificationTask;
 
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'T3CS.' . $_EXTKEY,
-	'Plan',
-	array(
-		'Session' => 'list, listPast, new, create, edit, update, delete',
-		'Room' => 'list',
-		'Slot' => 'list',
+    'T3CS.' . $_EXTKEY,
+    'Plan',
+    [
+        'Session' => 'list, listPast, new, create, edit, update, delete',
+        'Room' => 'list',
+        'Slot' => 'list',
 
-	),
-	// non-cacheable actions
-	array(
-		'Session' => 'create, update, delete',
-		'Room' => '',
-		'Slot' => '',
-
-	)
+    ],
+    [
+        'Session' => 'create, update, delete'
+    ]
 );
 
 // Session device service
